64999fb61ef34b6ea57cacd52f9589d4b0361a29
Siyu Guo <enyacat@gmail.com>
2022-02-23 00:29:46 +1100

    beautify next button

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -5,7 +5,20 @@ const Create = () => {
     return (
         <div className='create'>
             <h2>Add an event</h2>
-            <div className='next-btn'><Link className="create-btn" to="/event">Next</Link></div>
+            <div className='next-btn'
+                style={{
+                    '--color-1': 'deepskyblue',
+                    '--color-2': 'navy',
+                    background: `
+                      linear-gradient(
+                        170deg,
+                        var(--color-1),
+                        var(--color-2) 80%
+                      )
+                    `}}
+            ><div className="next-btn-icon" style={{
+                backgroundImage: `url("/images/send.svg")`
+            }}></div><Link to="/event">Next</Link></div>
         </div>
     );
 }
