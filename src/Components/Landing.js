8fd16f098986306638cf8f41ec324a88c23b3e6e
Siyu Guo <enyacat@gmail.com>
2022-02-22 21:31:12 +1100

    clear template, add image folder

---
+++ src/Components/Landing.js
@@ -0,0 +1,18 @@
+import "./Landing.css"
+
+
+export default function Landing() {
+
+    return (
+        <div className="main" style={{
+            backgroundImage: `url("/images/landing/Landing\ page\ background\ gradient.png")`
+        }}>
+            <div className="envited">envited</div>
+
+
+
+
+
+        </div>
+    )
+}
